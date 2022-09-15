2c6a4181c87586d13aa0681c69afc8b088ad396f
CADIC Jean-Maximilien <j.cadic@protonmail.ch>
2022-09-15 13:26:50 +0900

    code refactoring

--- setup.py
+++ setup.py
@@ -10,10 +10,12 @@ setup(
     url="https://github.com/JeanMaximilienCadic/google_dns",
     license="MIT",
     author="Jean Maximilien Cadic",
+    long_description="".join(open("README.md", "r").readlines()),
+    long_description_content_type="text/markdown",
     python_requires=">=3.6",
     install_requires=[r.rsplit()[0] for r in open("requirements.txt")],
     author_email="git@cadic.jp",
-    description="GNU Tools for python",
+    description="Google DNS",
     classifiers=[
         "Programming Language :: Python :: 3.6",
         "License :: OSI Approved :: MIT License",
